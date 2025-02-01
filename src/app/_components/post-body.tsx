dc42136637b3e3e53b286fbe89db0ccc0c3fb17f
t3om5 <bamboooo511@gmail.com>
2025-02-02 08:54:07 +0900

    Zenn CSS

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,16 +1,15 @@
-import markdownStyles from "./markdown-styles.module.css";
+import "zenn-content-css"
 
 type Props = {
-  content: string;
-};
+  content: string
+}
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="mx-auto mt-16 znc">
       <div
-        className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
-  );
+  )
 }
